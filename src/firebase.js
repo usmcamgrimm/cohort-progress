4da99c08a3efbe31718327d690ba61b4ae3accbc
Adrian Grimm <usmcamgrimm@gmail.com>
2022-02-12 14:42:53 -0500

    Update the firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,8 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// require('dotenv').config();
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
